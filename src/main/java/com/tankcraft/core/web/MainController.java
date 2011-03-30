bac8219b10031732b12f7b97857210859a815225
Michael Chen <mechiland@gmail.com>
2011-03-30 23:11:40 +0800

    initial commit, make the basic environment works.

---
+++ src/main/java/com/tankcraft/core/web/MainController.java
@@ -0,0 +1,15 @@
+package com.tankcraft.core.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public ModelAndView root() {
+        return new ModelAndView("index");
+    }
+
+}
